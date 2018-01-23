3caf0ce5e793d47185c913c36ca37985eb39e5d5
Daniel Rejon <rejonpardenilla@gmail.com>
2018-01-23 13:20:58 -0600

    wip: add contact form

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
@@ -10,3 +12,5 @@ new Vue({
   router,
   render: h => h(App)
 })
+
+Vue.use(VueAxios, axios)
