31bda6b3374dc8345b9d8c9834fa864e20257e75
Erick A. Montañez <codeams@gmail.com>
2018-01-23 19:25:21 -0600

    Replace project base
    Use codeams.me project base setup to initialize foundation. Replace html
    with pug (I also installed pug obviously). Remove default components and
    routes.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
-<template>
-  <div id="app">
-    <img src="./assets/images/logo.png">
-    <router-view/>
-  </div>
+<template lang='pug'>
+#app
 </template>
 
 <script>
@@ -13,12 +10,4 @@ export default {
 
 <style lang='scss'>
 @import 'app';
-
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
