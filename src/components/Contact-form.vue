2cc1a287eded299bdbec6b91fccb60843c71d721
Daniel Rejon <rejonpardenilla@gmail.com>
2018-03-10 02:46:30 -0600

    Clean form inputs

--- src/components/Contact-form.vue
+++ src/components/Contact-form.vue
@@ -43,13 +43,22 @@ export default {
               title: 'Gracias por contactarnos',
               text: 'Nos comunicaremos contigo lo más pronto posible',
               type: 'success'
-            })
+            }).then(() => { this.resetInputs() })
           })
           .catch(() => { this.hasError = true })
       } else {
         this.hasError = true
       }
       setTimeout(() => { this.hasError = false }, 820)
+    },
+    resetInputs () {
+      this.lead = {
+        name: '',
+        company: '',
+        email: '',
+        message: '',
+        createdAt: Date.now()
+      }
     }
   }
 }
@@ -87,7 +96,7 @@ export default {
   }
 
   .shake {
-    animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+    animation: shake-animation 0.82s cubic-bezier(.36,.07,.19,.97) both;
     transform: translate3d(0, 0, 0);
     backface-visibility: hidden;
     perspective: 1000px;
@@ -97,24 +106,6 @@ export default {
     @include button($expand: true);
     margin-top: 15px;
     padding: 8px 0;
-  }
-
-  @keyframes shake {
-    10%, 90% {
-      transform: translate3d(-1px, 0, 0);
-    }
-
-    20%, 80% {
-      transform: translate3d(2px, 0, 0);
-    }
-
-    30%, 50%, 70% {
-      transform: translate3d(-4px, 0, 0);
-    }
-
-    40%, 60% {
-      transform: translate3d(4px, 0, 0);
-    }
   }
 }
 </style>
