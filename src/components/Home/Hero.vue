b1da26390f254d5e58c2de6b43cbd6dd253eb76c
Erick A. Montañez <codeams@gmail.com>
2018-09-20 17:41:09 -0500

    Add bootstrap 4

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -3,3 +3,10 @@
     .first.phrase {{ $t('hero.title') }}
     .second.phrase {{ $t('hero.subtitle') }}
 </template>
+
+<style lang='sass'>
+  .phrase
+    color: red
+    @include media-breakpoint-up (md)
+      color: blue
+</style>
