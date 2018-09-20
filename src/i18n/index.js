1a43b299e03b8b3004f57e8e43e65d596524be84
Erick A. Montañez <codeams@gmail.com>
2018-09-20 02:33:45 -0500

    Improve i18n testability

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -4,15 +4,9 @@ import VueI18n from 'vue-i18n'
 // Locales
 import en from './en.json'
 
+export const locale = 'en'
+export const messages = { en }
+
 Vue.use(VueI18n)
-
-const messages = {
-  en
-}
-
-const i18n = new VueI18n({
-  locale: 'en',
-  messages
-})
-
+const i18n = new VueI18n({ locale, messages })
 export default i18n
--- tests/e2e/specs/home.spec.js
+++ tests/e2e/specs/home.spec.js
@@ -1,12 +1,15 @@
 
+import { messages, locale } from '../../../src/i18n'
+const t = messages[locale]
+
 describe('Hero', () => {
   it('Shows main heading first phrase', () => {
     cy.visit('/')
-    cy.contains('We boost businesses')
+    cy.contains(t.hero.title)
   })
 
   it('Shows main heading first phrase', () => {
     cy.visit('/')
-    cy.contains('taking them to the cloud')
+    cy.contains(t.hero.subtitle)
   })
 })
