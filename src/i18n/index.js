7df31b63134448ff58d37903280c253e6be72c1b
Erick A. Montañez <codeams@gmail.com>
2018-09-26 18:31:32 -0500

    Add i18n fallback locale

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -5,12 +5,13 @@ import VueI18n from 'vue-i18n'
 import en from './en.json'
 import es from './es.json'
 
-export const locale = 'en'
+export const locale = 'es'
+export const fallbackLocale = 'en'
 export const messages = {
   en,
   es
 }
 
 Vue.use(VueI18n)
-const i18n = new VueI18n({ locale, messages })
+const i18n = new VueI18n({ locale, fallbackLocale, messages })
 export default i18n
